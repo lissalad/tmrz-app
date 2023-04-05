4aec5b70619c50583418e50c98143559d4e82b19
lissalad <elissalaymon@gmail.com>
2023-04-05 11:40:30 -0700

    electron sorta working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import NewTimer from "./components/NewTimer";
 import ListTimers from "./components/ListTimers";
-import throttle from "lodash/throttle";
+// import throttle from "lodash/throttle";
 
 function App() {
   return (
